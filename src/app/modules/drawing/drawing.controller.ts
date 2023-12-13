407de029db1543eb124abd8977e7cc17406b4660
f4faysal <f4faysals@gmail.com>
2023-12-14 02:04:45 +0600

    Add drawing controller functions

---
+++ src/app/modules/drawing/drawing.controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { IDrawing } from './drawing.interface';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body;
+  const result = await DrawingService.insertIntoDB(data);
+  sendResponse<IDrawing>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Drawing Create Succssfully!',
+    data: result,
+  });
+});
+
+const getAllData = catchAsync(async (req: Request, res: Response) => {
+  const result = await DrawingService.getAllData();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'All Data Retrived Succssfully!',
+    data: result,
+  });
+});
+
+const getSingleData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DrawingService.getSingleData(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'All Data Retrived Succssfully!',
+    data: result,
+  });
+});
+
+const updateData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const result = await DrawingService.updateData(id, data);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Data Updated Succssfully!',
+    data: result,
+  });
+});
+
+const deleteData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DrawingService.deleteData(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Data Deleted Succssfully!',
+    data: result,
+  });
+});
+
+export const DrawingController = {
+  insertIntoDB,
+  getAllData,
+  getSingleData,
+  updateData,
+  deleteData,
+};
